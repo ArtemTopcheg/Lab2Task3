2a1ed558bde7bbf9fac217ddee6972718b63bd2d
Артём Шустов < @DESKTOP-3G11VV9>
2022-09-26 21:32:13 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Task3
+
+int[] nums = new int[] { 1, 2, 3, 4, 5, -3, -4, -10 };
+int summ = 0;
+for (int i = 0; i < nums.Length; i++)
+{
+    if (nums[i] > 0)
+    {
+        summ += nums[i];
+    }
+}
+Console.WriteLine(summ);
